cbbfe0fffe761e71a40042a8788d8a625384cc57
Dmitrij Patuk <dmitrij.patuk@gmx.de>
2024-12-08 17:04:52 +0100

    created recursive maze creation algorithm

--- cell.py
+++ cell.py
@@ -1,14 +1,20 @@
-
 from __future__ import annotations
 from line import Line
 from point import Point
 from window import Window
 
+
 class Cell:
-    def __init__(self, window:Window, 
-                 p1:Point = None, p2:Point = None, 
-                 right_wall:bool = True, left_wall:bool = True, 
-                 top_wall:bool = True, bottom_wall:bool = True):
+    def __init__(
+        self,
+        window: Window,
+        p1: Point = None,
+        p2: Point = None,
+        right_wall: bool = True,
+        left_wall: bool = True,
+        top_wall: bool = True,
+        bottom_wall: bool = True,
+    ):
         self.p1 = p1
         self.p2 = p2
         self.__window = window
@@ -16,88 +22,102 @@ class Cell:
         self.left_wall = left_wall
         self.top_wall = top_wall
         self.bottom_wall = bottom_wall
-    
-    def draw(self, line_color = "black"):
-        
+        self.visited = False
+
+    def draw(self, line_color="black"):
+
         tl = self.p1
         bl = Point(self.p1.x, self.p2.y)
         if self.left_wall:
-            self.__window.draw_line(Line(tl,bl), line_color)
+            self.__window.draw_line(Line(tl, bl), line_color)
         else:
-            self.__window.draw_line(Line(tl,bl), "white")
-        
+            self.__window.draw_line(Line(tl, bl), "white")
+
         tl = self.p1
         tr = Point(self.p2.x, self.p1.y)
         if self.top_wall:
-            self.__window.draw_line(Line(tl,tr), line_color)
+            self.__window.draw_line(Line(tl, tr), line_color)
         else:
-            self.__window.draw_line(Line(tl,tr), "white")
-        
+            self.__window.draw_line(Line(tl, tr), "white")
+
         bl = Point(self.p1.x, self.p2.y)
         br = Point(self.p2.x, self.p2.y)
         if self.bottom_wall:
-            self.__window.draw_line(Line(bl,br), line_color)
+            self.__window.draw_line(Line(bl, br), line_color)
         else:
-            self.__window.draw_line(Line(bl,br), "white")
-            
+            self.__window.draw_line(Line(bl, br), "white")
+
         tl = Point(self.p2.x, self.p1.y)
         br = Point(self.p2.x, self.p2.y)
         if self.right_wall:
-            self.__window.draw_line(Line(tl,br), line_color)
+            self.__window.draw_line(Line(tl, br), line_color)
         else:
-            self.__window.draw_line(Line(tl,br), "white")
-    
+            self.__window.draw_line(Line(tl, br), "white")
+
     def draw_move(self, other: Cell, undo=False):
         line_color = "red"
         if undo:
             line_color = "gray"
         # to cell on right side
-        if (self.p1.y == other.p1.y and self.p2.y == other.p2.y and 
-            self.p1.x < other.p1.x and self.p2.x < other.p2.x):
-            cntrY = self.p1.y + ((self.p2.y - self.p1.y) / 2 )
-            strtX = self.p1.x + ((self.p2.x - self.p1.x) / 2 )
-            endX = other.p2.x - ((other.p2.x - other.p1.x) / 2 )
+        if (
+            self.p1.y == other.p1.y
+            and self.p2.y == other.p2.y
+            and self.p1.x < other.p1.x
+            and self.p2.x < other.p2.x
+        ):
+            cntrY = self.p1.y + ((self.p2.y - self.p1.y) / 2)
+            strtX = self.p1.x + ((self.p2.x - self.p1.x) / 2)
+            endX = other.p2.x - ((other.p2.x - other.p1.x) / 2)
             p1 = Point(strtX, cntrY)
             p2 = Point(endX, cntrY)
             l = Line(p1, p2)
-            print(f'To cell right side {l}')
+            print(f"To cell right side {l}")
             self.__window.draw_line(l, line_color)
-        
+
         ## to cell on left side
-        if (self.p1.y == other.p1.y and self.p2.y == other.p2.y and 
-            self.p1.x > other.p1.x and self.p2.x > other.p2.x):
-            cntrY = self.p1.y + ((self.p2.y - self.p1.y) / 2 )
-            strtX = self.p2.x - ((self.p2.x - self.p1.x) / 2 )
-            endX = other.p1.x + ((other.p2.x - other.p1.x) / 2 )
+        if (
+            self.p1.y == other.p1.y
+            and self.p2.y == other.p2.y
+            and self.p1.x > other.p1.x
+            and self.p2.x > other.p2.x
+        ):
+            cntrY = self.p1.y + ((self.p2.y - self.p1.y) / 2)
+            strtX = self.p2.x - ((self.p2.x - self.p1.x) / 2)
+            endX = other.p1.x + ((other.p2.x - other.p1.x) / 2)
             p1 = Point(strtX, cntrY)
             p2 = Point(endX, cntrY)
             l = Line(p1, p2)
-            print(f'To cell left side {l}')
+            print(f"To cell left side {l}")
             self.__window.draw_line(l, line_color)
-        
+
         # to cell above
-        if (self.p1.x == other.p1.x and self.p2.x == other.p2.x and 
-            self.p1.y > other.p1.y and self.p2.y > other.p2.y):
-            cntrX = self.p1.x + ((self.p2.x - self.p1.x) / 2 )
-            strtY = self.p2.y - ((self.p2.y - self.p1.y) / 2 )
-            endY = other.p1.y + ((other.p2.y - other.p1.y) / 2 )
+        if (
+            self.p1.x == other.p1.x
+            and self.p2.x == other.p2.x
+            and self.p1.y > other.p1.y
+            and self.p2.y > other.p2.y
+        ):
+            cntrX = self.p1.x + ((self.p2.x - self.p1.x) / 2)
+            strtY = self.p2.y - ((self.p2.y - self.p1.y) / 2)
+            endY = other.p1.y + ((other.p2.y - other.p1.y) / 2)
             p1 = Point(cntrX, strtY)
             p2 = Point(cntrX, endY)
             l = Line(p1, p2)
-            print(f'To cell above {l}')
+            print(f"To cell above {l}")
             self.__window.draw_line(l, line_color)
-        
+
         # to cell below
-        if (self.p1.x == other.p1.x and self.p2.x == other.p2.x and 
-            self.p1.y < other.p1.y and self.p2.y < other.p2.y):
-            cntrX = self.p1.x + ((self.p2.x - self.p1.x) / 2 )
-            strtY = self.p1.y + ((self.p2.y - self.p1.y) / 2 )
-            endY = other.p2.y - ((other.p2.y - other.p1.y) / 2 )
+        if (
+            self.p1.x == other.p1.x
+            and self.p2.x == other.p2.x
+            and self.p1.y < other.p1.y
+            and self.p2.y < other.p2.y
+        ):
+            cntrX = self.p1.x + ((self.p2.x - self.p1.x) / 2)
+            strtY = self.p1.y + ((self.p2.y - self.p1.y) / 2)
+            endY = other.p2.y - ((other.p2.y - other.p1.y) / 2)
             p1 = Point(cntrX, strtY)
             p2 = Point(cntrX, endY)
             l = Line(p1, p2)
-            print(f'To cell below {l}')
+            print(f"To cell below {l}")
             self.__window.draw_line(l, line_color)
-    
-            
-            
--- maze.py
+++ maze.py
@@ -1,4 +1,4 @@
-
+import random
 import time
 from cell import Cell
 from point import Point
@@ -6,48 +6,107 @@ from window import Window
 
 
 class Maze:
-    def __init__(self, p: Point, 
-                 num_rows, num_cols, 
-                 cell_size_x, cell_size_y, 
-                 window: Window):
+    def __init__(
+        self,
+        p: Point,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        window: Window,
+        seed=None,
+    ):
         self._p = p
         self._num_rows = num_rows
         self._num_cols = num_cols
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._window = window
+        if seed:
+            random.seed(seed)
         self._create_cells()
-        
+
     def _create_cells(self):
         self._cells = []
-        for y in range(self._num_rows): 
+        for y in range(self._num_rows):
             cols = []
             for x in range(self._num_cols):
                 cols.append(Cell(window=self._window))
             self._cells.append(cols)
-    
+
     def draw(self):
-        for y in range(self._num_rows): 
+        for y in range(self._num_rows):
             for x in range(self._num_cols):
-                self._draw_cell(x,y)
+                self._draw_cell(x, y)
         self._break_entrance_and_exit()
-    
+        self._break_walls_r(0,0)
+
     def _draw_cell(self, x, y):
-        p1 = Point(self._p.x + self._cell_size_x * x, 
-                   self._p.y + self._cell_size_y * y)
-        p2 = Point(self._p.x + self._cell_size_x * (x + 1), 
-                   self._p.y + self._cell_size_y * (y + 1))
+        p1 = Point(self._p.x + self._cell_size_x * x, self._p.y + self._cell_size_y * y)
+        p2 = Point(
+            self._p.x + self._cell_size_x * (x + 1),
+            self._p.y + self._cell_size_y * (y + 1),
+        )
         self._cells[x][y].p1 = p1
         self._cells[x][y].p2 = p2
         self._cells[x][y].draw()
         self._animate()
-    
+
     def _animate(self):
         self._window.redraw()
         time.sleep(0.03)
-        
+
     def _break_entrance_and_exit(self):
         self._cells[0][0].top_wall = False
-        self._draw_cell(0,0)
+        self._draw_cell(0, 0)
         self._cells[self._num_rows - 1][self._num_cols - 1].bottom_wall = False
-        self._draw_cell(self._num_rows - 1,self._num_cols - 1)
+        self._draw_cell(self._num_rows - 1, self._num_cols - 1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            next_index_list = []
+
+            # left cell
+            if i > 0 and not self._cells[i - 1][j].visited:
+                next_index_list.append((i - 1, j))
+            # right cell
+            if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
+                next_index_list.append((i + 1, j))
+            # top cell
+            if j > 0 and not self._cells[i][j - 1].visited:
+                next_index_list.append((i, j - 1))
+            # bottom cell
+            if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
+                next_index_list.append((i, j + 1))
+
+            # no where to go so breaak
+            if len(next_index_list) == 0:
+                self._draw_cell(i, j)
+                return
+
+            # choose a random direction to go
+            direction_index = random.randrange(len(next_index_list))
+            next_index = next_index_list[direction_index]
+
+            # right direction wall removal
+            if next_index[0] == i + 1:
+                self._cells[i][j].right_wall = False
+                self._cells[i + 1][j].left_wall = False
+            # left direction wall removal
+            if next_index[0] == i - 1:
+                self._cells[i][j].left_wall = False
+                self._cells[i - 1][j].right_wall = False
+            # down direction wall removal
+            if next_index[1] == j + 1:
+                self._cells[i][j].bottom_wall = False
+                self._cells[i][j + 1].top_wall = False
+            # up direction wall removal
+            if next_index[1] == j - 1:
+                self._cells[i][j].top_wall = False
+                self._cells[i][j - 1].bottom_wall = False
+
+            # visit next cell
+            self._break_walls_r(next_index[0], next_index[1])
+
+
