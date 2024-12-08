ccca4cd7f0b410d05b54ed3522694ca4bf7ce8ed
Dmitrij Patuk <dmitrij.patuk@gmx.de>
2024-12-08 13:46:47 +0100

    Added move drawing between cells, still needs drawing from center of cell to center of cell

--- cell.py
+++ cell.py
@@ -1,8 +1,8 @@
 
+from __future__ import annotations
 from line import Line
 from point import Point
 from window import Window
-
 
 class Cell:
     def __init__(self, p1:Point, p2:Point, window:Window,
@@ -16,23 +16,75 @@ class Cell:
         self.top_wall = top_wall
         self.bottom_wall = bottom_wall
     
-    def draw(self):
-        #
+    def draw(self, line_color = "black"):
         if self.left_wall:
             tl = self.__p1
             bl = Point(self.__p1.x, self.__p2.y)
-            self.__window.draw_line(Line(tl,bl), "red")
+            self.__window.draw_line(Line(tl,bl), line_color)
         if self.top_wall:
             tl = self.__p1
             tr = Point(self.__p2.x, self.__p1.y)
-            self.__window.draw_line(Line(tl,tr), "red")
+            self.__window.draw_line(Line(tl,tr), line_color)
         if self.bottom_wall:
             bl = Point(self.__p1.x, self.__p2.y)
             br = Point(self.__p2.x, self.__p2.y)
-            self.__window.draw_line(Line(bl,br), "red")
+            self.__window.draw_line(Line(bl,br), line_color)
         if self.right_wall:
             tl = Point(self.__p2.x, self.__p1.y)
             br = Point(self.__p2.x, self.__p2.y)
-            self.__window.draw_line(Line(tl,br), "red")
+            self.__window.draw_line(Line(tl,br), line_color)
+            
+    def draw_move(self, other: Cell, undo=False):
+        line_color = "red"
+        if undo:
+            line_color = "gray"
+        # to cell on right side
+        if (self.__p1.y == other.__p1.y and self.__p2.y == other.__p2.y and 
+            self.__p1.x < other.__p1.x and self.__p2.x < other.__p2.x):
+            cntrY = self.__p1.y + ((self.__p2.y - self.__p1.y) / 2 )
+            strtX = self.__p1.x
+            endX = other.__p2.x
+            p1 = Point(strtX, cntrY)
+            p2 = Point(endX, cntrY)
+            l = Line(p1, p2)
+            print(f'To cell right side {l}')
+            self.__window.draw_line(l, line_color)
+        
+        ## to cell on left side
+        if (self.__p1.y == other.__p1.y and self.__p2.y == other.__p2.y and 
+            self.__p1.x > other.__p1.x and self.__p2.x > other.__p2.x):
+            cntrY = self.__p1.y + ((self.__p2.y - self.__p1.y) / 2 )
+            strtX = self.__p2.x
+            endX = other.__p1.x
+            p1 = Point(strtX, cntrY)
+            p2 = Point(endX, cntrY)
+            l = Line(p1, p2)
+            print(f'To cell left side {l}')
+            self.__window.draw_line(l, line_color)
+        
+        # to cell above
+        if (self.__p1.x == other.__p1.x and self.__p2.x == other.__p2.x and 
+            self.__p1.y > other.__p1.y and self.__p2.y > other.__p2.y):
+            cntrX = self.__p1.x + ((self.__p2.x - self.__p1.x) / 2 )
+            strtY = self.__p2.y
+            endY = other.__p1.y
+            p1 = Point(cntrX, strtY)
+            p2 = Point(cntrX, endY)
+            l = Line(p1, p2)
+            print(f'To cell above {l}')
+            self.__window.draw_line(l, line_color)
+        
+        # to cell below
+        if (self.__p1.x == other.__p1.x and self.__p2.x == other.__p2.x and 
+            self.__p1.y < other.__p1.y and self.__p2.y < other.__p2.y):
+            cntrX = self.__p1.x + ((self.__p2.x - self.__p1.x) / 2 )
+            strtY = self.__p1.y
+            endY = other.__p2.y
+            p1 = Point(cntrX, strtY)
+            p2 = Point(cntrX, endY)
+            l = Line(p1, p2)
+            print(f'To cell below {l}')
+            self.__window.draw_line(l, line_color)
+    
             
             
--- line.py
+++ line.py
@@ -10,3 +10,6 @@ class Line:
         c.create_line(self.__p1.x, self.__p1.y, 
                       self.__p2.x, self.__p2.y, 
                       fill=fill_color, width=2)
+        
+    def __str__(self):
+        return f'p1: {self.__p1} p2:{self.__p2}' 
